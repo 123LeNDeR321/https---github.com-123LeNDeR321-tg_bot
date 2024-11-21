71c2d5d8ea014b160d69df364cb5c016c5ba08ba
123LeNDeR321 <anrej.sheboldaev@mail.ru>
2024-11-21 15:29:56 +0300

    v2

--- main.py
+++ main.py
@@ -4,19 +4,30 @@ from aiogram.enums import ParseMode
 from aiogram.types import Message
 import asyncio
 import logging
-from config import TOKEN  #токен бота
+from config import TOKEN, OWNER_ID  #токен бота
 
 TOKEN_API = TOKEN
 
+bot = Bot(token=TOKEN_API, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
+dp = Dispatcher()
 async def get_start(message: Message):
-    await message.reply(f"<b>Хай {message.from_user.first_name}. Прости за прошлое.</b>")
+    if "svo" in message.text.lower():
+        await message.reply(f"<b>ZOV\nZOV\nZOV</b>")
+    else:
+        await message.reply(f"<b>Хай {message.from_user.first_name}. Прости за прошлое.</b>")
+    
+async def bot_start():
+    await bot.send_message(chat_id=OWNER_ID, text="Бот запущен")
 
+async def bot_stop():
+    await bot.send_message(chat_id=OWNER_ID , text="<b>Бот остановлен</b>")
 
 
 async def start():
-    bot = Bot(token=TOKEN_API, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    dp = Dispatcher()
+    
+    dp.startup.register(bot_start)
+    dp.shutdown.register(bot_stop)
     dp.message.register(get_start)
     try:
         await dp.start_polling(bot)
