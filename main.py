291d73a4feb37c1e536a57d1331b4119470f84ae
123LeNDeR321 <anrej.sheboldaev@mail.ru>
2024-11-27 14:49:01 +0300

    v2.4

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.bot import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.types import Message
+from aiogram.filters import*
 import asyncio
 import logging
 from config import TOKEN, OWNER_ID  #токен бота
 from game import*
+from comand import*
 
 
 
@@ -19,17 +21,32 @@ TOKEN_API = TOKEN
 bot = Bot(token=TOKEN_API, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.reply("Привет, я Svой, не бойся, что-бы узнать меня получше, напиши /help")
+
+@dp.message(Command(zov_comand))
+async def cmd_zov(message: Message):
+    await message.reply("<b>Слышу зов, ебать азов</b>")
+
+@dp.message(Command(help_comand))
+async def cmd_help(message: Message):
+    await message.reply(f"<b>Команды:\n/{start_comand}\n/{help_comand}\n/{zov_comand}\n/{svo_comand}\nя улучшаюсь, поэтому этот список увеличится :)</b>")
+
+@dp.message(Command(svo_comand))
+async def cmd_svo(message: Message):
+    await message.answer("<b>СВО, ГОЙДА БРАТЬ, Я ВАС ЛЮБЛЮ</b>")
+
 async def get_start(message: Message):
     keys = ', '.join(list(game_dictionari.keys()))
 
-    if "svo" in message.text.lower():
-        await message.reply("Goida \n Svo \n Slava Geroym")
-
     if message.text.lower() in game_dictionari.keys():
-        await message.reply(f"Возможно тебе поняравятся эти игры:\n {', '.join(game_dictionari[message.text.lower()])}")
-    
+        await message.reply(f"Возможно тебе понравятся эти игры:\n{', '.join(game_dictionari[message.text.lower()])}")
+    elif message.text.lower() == "иди нахуй":
+        await message.reply("<b>Ты чё падаль, попутал мальца?</b>")
     else:
-        await message.reply(f"<b>Хай {message.from_user.first_name}. \n Напиши жанр который тебе нравится из приведённых: {keys}</b>")
+        await message.reply(f"<b>Хай {message.from_user.first_name}.\nНапиши жанр который тебе нравится из приведённых: {keys}</b>")
     
 async def bot_start():
     await bot.send_message(chat_id=OWNER_ID, text="<b>Бот запущен</b>")
